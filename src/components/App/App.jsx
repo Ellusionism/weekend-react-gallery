330a9f607f1f13f95a7f9544fde3cc3bef2665f4
Mike Schwartz <michael.schwartz001@gmail.com>
2023-01-15 17:46:44 -0600

    added server GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [galleryItems, setGalleryItems] = useState([]);
+
+useEffect(() => {
+  getGalleryData();
+}, []);
+
+const getGalleryData = () => {
+  axios({
+    method: 'GET',
+    url: '/gallery'
+  }).then((response) => {
+    console.log(response.data)
+    const getData = response.data
+    setGalleryItems(getData);
+  }).catch((error) => {
+    console.log('Error in getGalleryData:', error)
+  });
+};
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
       </div>
     );
 }
