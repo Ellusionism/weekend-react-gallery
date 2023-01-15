0c0a3b8282cc4622d6fa664246893dd2d0490029
Mike Schwartz <michael.schwartz001@gmail.com>
2023-01-15 17:18:40 -0600

    added images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: '../../public/images/arches_formation.jpeg', description: 'A unique rock formation, Arches National Park, Utah.', likes: 0 },
+    { id: 2, path: '../../public/images/badlands_wildfire.jpeg', description: 'The beginnings of a wildfire, Badlands National Park, South Dakota.', likes: 0 },
+    { id: 3, path: '../../public/images/bryce_canyon_amphitheater.jpeg', description: 'A panoramic view of Bryce Amphitheater, Bryce Canyon National Park, Utah.', likes: 0 },
+    { id: 4, path: '../../public/images/crater_lake_clouds.jpeg', description: 'Clouds hanging low over Crater Lake, Crater Lake National Park, Oregon.', likes: 0 },
+    { id: 5, path: '../../public/images/grand_canyon_rim.jpeg', description: 'Looking down at the Colorado River from the canyon rim, Grand Canyon National Park, Arizona.', likes: 0 },
+    { id: 6, path: '../../public/images/rocky_mountains_rest_stop.jpeg', description: 'View from a rest stop en route to Yellowstone National Park, Rocky Mountain Range, Montana.', likes: 0 }
 ];
 
 module.exports = galleryItems;
